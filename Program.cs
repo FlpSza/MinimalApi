d94ed8226adb65cc3562b63bc8bc61e61d36700e
Fellipe Souza <ofellipe2023@gmail.com>
2024-08-19 14:06:31 -0300

    Base projeto

--- Program.cs
+++ Program.cs
@@ -1,11 +1,22 @@
 using Microsoft.AspNetCore.SignalR;
+using minimalApi.Infraestrutura.Db;
+using minimalApi.Dominio.DTOs;
+using Microsoft.Extensions.Options;
+using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddDbContext<DbContexto>(options => {
+    options.UseMySql(
+        builder.Configuration.GetConnectionString("mysql"),
+        ServerVersion.AutoDetect(builder.Configuration.GetConnectionString("mysql")));
+});
+
 var app = builder.Build();
 
 app.MapGet("/", () => "Hello World!");
 
-app.MapPost("/login", (minimalApi.Dominio.DTOs.LoginDTO loginDTO) => {
+app.MapPost("/login", (LoginDTO loginDTO) => {
 
 if(loginDTO.Email == "adm@teste.com" && loginDTO.Senha == "123456")
     return Results.Ok("Login realizado");
